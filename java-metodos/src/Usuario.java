12ce769f02bf313f1119989c5ddbadbad8e55182
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-12 20:20:47 -0300

    Requisito o medto

--- java-metodos/src/Usuario.java
+++ java-metodos/src/Usuario.java
@@ -2,6 +2,12 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        smartTv.aumentarVolume();
+
         System.out.println("TV Ligada ? " + smartTv.ligada);
         System.out.println("Canal atual :" + smartTv.canal);
         System.out.println("Volume atual :" + smartTv.volume);
