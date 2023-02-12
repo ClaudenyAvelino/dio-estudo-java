f8b175dda1b51aab57b626325dd179a6030d40e7
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-12 18:21:18 -0300

    inicio estudos

---
+++ Java-inicial/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numroNormal = numeroCurto;
+        Short numeroCurto2 = (short) numroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+}
