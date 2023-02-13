75a302179762258937efc95cb98554c8b98adbb7
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-13 15:08:31 -0300

    Projeto logica-java

---
+++ java-logica/src/Logica.java
@@ -0,0 +1,32 @@
+public class Logica {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 10;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "fulano";
+        String s2 = "fulano";
+        String s3 = "cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8997L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2 "+ (i1 == i2));
+        System.out.println("i1 != i2 "+ (i1 != i2));
+        System.out.println("i1 > i2 "+ (i1 > i2));
+        System.out.println("i1 <= i2 "+ (i1 <= i2));
+        System.out.println(" ");
+        System.out.println("f1 == i2 "+ (f1 == f2));
+        System.out.println("f1 != i2 "+ (f1 != f2));
+        System.out.println("f1 > i2 "+ (f1 > f2));
+        System.out.println("f1 <= i2 "+ (f1 <= f2));
+
+    }
+}
