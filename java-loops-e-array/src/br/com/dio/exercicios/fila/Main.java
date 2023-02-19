2ca6eb5f566dd527585cdd27be68499c5d8c3c39
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-19 20:58:00 -0300

    Projeto estrura de dados teste fila

---
+++ java-loops-e-array/src/br/com/dio/exercicios/fila/Main.java
@@ -0,0 +1,4 @@
+package br.com.dio.exercicios.fila;
+
+public class Main {
+}
---
+++ java-loops-e-array/src/br/com/dio/exercicios/pilha/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.exercicios.pilha;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pilha minhaPilha = new Pilha();
+
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+        minhaPilha.push(new No(4));
+        minhaPilha.push(new No(5));
+
+        System.out.println(minhaPilha);
+
+        System.out.println(minhaPilha.pop());
+
+        System.out.println(minhaPilha);
+
+        minhaPilha.push(new No(99));
+
+        minhaPilha.push(new No(1));
+
+
+    }
+}
