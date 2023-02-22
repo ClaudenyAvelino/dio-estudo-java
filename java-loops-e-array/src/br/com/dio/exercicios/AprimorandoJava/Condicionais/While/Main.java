72490b562eb93b9d7ba504920fb09460b025aac2
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-22 17:34:27 -0300

    Exercicio Laço (while)

---
+++ java-loops-e-array/src/br/com/dio/exercicios/AprimorandoJava/Condicionais/While/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio.exercicios.AprimorandoJava.Condicionais.While;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int cout = 0;
+        while (cout <= 10) {
+            System.out.println("contador " + cout);
+
+            cout = cout + 1;
+        }
+    }
+}
