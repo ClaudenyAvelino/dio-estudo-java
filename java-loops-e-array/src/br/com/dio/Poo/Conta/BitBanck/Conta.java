bc5d1e0a9ccb2964a0d5aa70461f23c73c54fe5b
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2023-02-24 18:37:20 -0300

    revisão POO atualização classe conta

--- java-loops-e-array/src/br/com/dio/Poo/Conta/BitBanck/Conta.java
+++ java-loops-e-array/src/br/com/dio/Poo/Conta/BitBanck/Conta.java
@@ -1,10 +1,21 @@
 package br.com.dio.Poo.Conta.BitBanck;
 
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total =0;
+
+    public Conta(int agencia, int numero){
+        Conta.total++;
+        System.out.println("O total é " + Conta.total);
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100;
+        System.out.println("Estou criando uma conta "+this.numero);
+
+    }
 
     public void deposita(double valor){
             this.saldo += valor;
@@ -26,5 +37,45 @@ public class Conta {
         }else {
             return false;
         }
+    }
+
+    public double getSaldo() {
+        return this.numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia = agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia <= 0){
+            System.out.println("Não pode valor manor igual a 0");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public void getNumero(int numero) {
+        if (numero <= 0){
+            System.out.println("Não pode valor manor igual a 0");
+            return ;
+        }
+        this.numero = numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
     }
 }
--- java-loops-e-array/src/br/com/dio/Poo/Conta/BitBanck/TestaBanco.java
+++ java-loops-e-array/src/br/com/dio/Poo/Conta/BitBanck/TestaBanco.java
@@ -3,20 +3,15 @@ package br.com.dio.Poo.Conta.BitBanck;
 public class TestaBanco {
 
     public static void main(String[] args) {
-        Cliente clau = new Cliente();
-        clau.nome  ="claudeny";
-        clau.cpf = "222.222.222-22";
-        clau.profissao = "prog";
+       Conta conta = new Conta(1337, 23334);
+       Conta conta1 = new Conta(1021, 2233);
+       Conta conta2 = new Conta(1022, 2233);
+       Conta conta3 = new Conta(1023, 2233);
 
-
-        Conta contaDoPaulo = new Conta();
-        contaDoPaulo.deposita(100);
-
-        contaDoPaulo.titular = clau;
-        System.out.println(contaDoPaulo.titular.nome);
-        System.out.println(contaDoPaulo.titular);
-        System.out.println(clau.nome);
-
-
+       conta.deposita(200.0);
+        System.out.println(conta.getSaldo());
+        conta.setAgencia(570);
+        System.out.println(conta.getAgencia());
+        System.out.println("o total de contas é : "+ Conta.getTotal());
     }
 }
